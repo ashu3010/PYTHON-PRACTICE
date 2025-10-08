a224f3c81be781c5bd26612562cea32c96980dbf
ashu3010 <85890654+ashu3010@users.noreply.github.com>
2025-10-08 10:28:33 +0530

    Add files via upload

---
+++ CONDITIONAL STATEMENT SET-3/Q1_IF_ELSE.py
@@ -0,0 +1,36 @@
+# Write a program that asks the user for a number and prints whether it is positive, negative, or zero.
+'''number = int(input("Enter your number:"))
+print(number)
+
+if(number > 0):
+    print("Your no. is Positive.")
+
+elif(number<0):
+    print("Your no. is Negetive.")
+else:
+    print("your no. is zero")
+'''
+
+
+# Create a program that checks if a person is eligible to vote (age >= 18).
+'''age= int(input("What is your age?\n"))
+
+if(age<=0):
+    print("this is not your age.")
+elif(age<18):
+    print("You are not eligible for voting now.\nTry again after"+ str(age-18)+ "years.")
+else:
+    print("You are eligible for voting.")
+   ''' 
+
+
+
+# Write a program that takes a number from the user and prints "Even" if it is even, otherwise "Odd".
+
+'''num= int(input("Please provide a number.\n"))
+
+if(num/2==0):
+    print("EVEN")
+else:
+    print("ODD")
+   ''' 
---
+++ CONDITIONAL STATEMENT SET-3/Q2_match_case.py
@@ -0,0 +1,38 @@
+ # Ask the user to enter a day number (1–7) and print the corresponding day of the week using match case.
+'''day= int(input("Enter ypur weekday number:"))
+match day:
+    case 1 :
+        print("SUNDAY")
+    case 2 :
+        print("MONDAY")
+    case 3 :
+        print("TUESDAY")
+    case 4 :
+        print("WEDNESDAY")
+    case 5 :
+        print("THURSDAY")
+    case 6 :
+        print("FRIDAY")
+    case 7 :
+        print("SATURDAY")'''
+        
+
+
+# Write a program using match case that simulates a simple calculator.
+# 1)Ask the user for two numbers and an operation (+, -, *, /).
+# 2)Perform the operation using match case.
+
+'''num1=int(input("Enter 1st no. :"))
+num2=int(input("Enter 2nd no. :"))
+
+operation=input("Enter your operation:")
+
+match operation:
+    case "+":
+        print(num1 + num2)
+    case "-":
+        print(num1-num2)
+    case "*":
+        print(num1*num2)
+    case "/":
+        print(num1/num2)'''
